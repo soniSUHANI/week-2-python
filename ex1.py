3c48beb8a1bd905e5f90b73b2487e708127ed39d
soniSUHANI <114800693+soniSUHANI@users.noreply.github.com>
2022-12-12 19:52:45 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,5 @@
+total = 0
+num= input("Enter a number: ")
+for i in range (0,len(num)):
+    total =total+ int(num[i])
+print(total)
---
+++ ex5.py
@@ -0,0 +1,12 @@
+for i in range(1,11,2):
+    print(i)
+for i in range(10,0,-1):
+    print(i)
+for i in range(1,-11,-1):
+    print(i)
+
+num = input("enter a number: ")
+total = 0
+for i in num:
+    total += int(i)
+print(total)
